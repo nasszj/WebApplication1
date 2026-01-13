09a201b2a50fa73dfe538a59c038a50bf5fba5c9
amberagnihotri <amberagnihotri0@gmail.com>
2026-01-13 10:31:16 +0100

    navbar veranderingen + pagina's

---
+++ WebApplication1/Controllers/StatistiekenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StatistiekenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Views/Statistieken/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.Statistieken
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
